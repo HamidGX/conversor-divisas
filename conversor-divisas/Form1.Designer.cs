2d715b5097910118d182f13f49a8a1fe59f9660f
HamidGX <hamid.uxw@gmail.com>
2022-02-09 21:24:16 -0600

    Se crearon los archivos del proyecto

---
+++ conversor-divisas/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace conversor_divisas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cmbDivisas = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbDivisas
+            // 
+            this.cmbDivisas.BackColor = System.Drawing.SystemColors.Window;
+            this.cmbDivisas.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbDivisas.FormattingEnabled = true;
+            this.cmbDivisas.ItemHeight = 29;
+            this.cmbDivisas.Items.AddRange(new object[] {
+            "Pesos Mexicanos (MXN)",
+            "Pesos Argentinos (ARS)",
+            "Pesos Colombianos (COP)",
+            "Dólar (USD)",
+            "Dólar Canadiense (CAD)",
+            "Dólar de Nueva Zelanda (NZD)",
+            "Euro (EUR)"});
+            this.cmbDivisas.Location = new System.Drawing.Point(245, 160);
+            this.cmbDivisas.Name = "cmbDivisas";
+            this.cmbDivisas.Size = new System.Drawing.Size(234, 37);
+            this.cmbDivisas.TabIndex = 0;
+            this.cmbDivisas.SelectedIndexChanged += new System.EventHandler(this.cmbDivisas_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.IntegralHeight = false;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Pesos Mexicanos (MXN)",
+            "Pesos Argentinos (ARS)",
+            "Pesos Colombianos (COP)",
+            "Dólar (USD)",
+            "Dólar Canadiense (CAD)",
+            "Dólar de Nueva Zelanda (NZD)",
+            "Euro (EUR)"});
+            this.comboBox2.Location = new System.Drawing.Point(245, 211);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(234, 37);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(141, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Moneda";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(217, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Moneda a convertir";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCantidad.Location = new System.Drawing.Point(246, 115);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(233, 33);
+            this.txtCantidad.TabIndex = 5;
+            this.txtCantidad.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(245, 262);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(234, 33);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(131, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Cantidad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(120, 266);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 29);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Resultado";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(120, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 35);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Limpiar";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(489, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 35);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "= Calcular";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(322, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(84, 79);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(669, 398);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.cmbDivisas);
+            this.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conversor de Divisas";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbDivisas;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ conversor-divisas/Form1.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Programmed on earth by humans.
+//Hamid Cuevas Escalera.
+namespace conversor_divisas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void cmbDivisas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          if (cmbDivisas.Text== "Pesos Mexicanos (MXN)")
+
+            {
+                switch (comboBox2.Text)
+
+                {
+                   
+                    case "Pesos Mexicanos (MXN)":
+                    MessageBox.Show("Error: No debes ingresar la misma moneda.");
+                    break;
+
+                    case  "Pesos Argentinos (ARS)":
+                        double monedaArg = double.Parse(txtCantidad.Text);
+                        monedaArg = monedaArg * 4.84;
+                        textBox2.Text = monedaArg.ToString();
+                        break;
+
+                    case "Pesos Colombianos (COP)":
+                        double monedaCop = double.Parse(txtCantidad.Text);
+                        monedaCop = monedaCop * 189.25;
+                        textBox2.Text = monedaCop.ToString();
+                        break;
+
+                    case "Dólar (USD)":
+                        double monedaUsd = double.Parse(txtCantidad.Text);
+                        monedaUsd = monedaUsd * 0.051;
+                        textBox2.Text = "$" + monedaUsd.ToString(); 
+                        break;
+
+                    case "Dólar Canadiense (CAD)":
+                        double monedaCad = double.Parse(txtCantidad.Text);
+                        monedaCad = monedaCad * 0.062;
+                        textBox2.Text = monedaCad.ToString();
+                        break;
+
+                    case "Dólar de Nueva Zelanda (NZD)":
+                        double monedaNzd = double.Parse(txtCantidad.Text);
+                        monedaNzd = monedaNzd * 0.072;
+                        textBox2.Text = monedaNzd.ToString();
+                        break;
+                    
+                    case "Euro (EUR)":
+                        double monedaEur = double.Parse(txtCantidad.Text);
+                        monedaEur = monedaEur * 0.043;
+                        textBox2.Text = "€" + monedaEur.ToString();
+                        break;
+
+
+                }
+            }
+          if (cmbDivisas.Text == "Pesos Argentinos (ARS)")
+            {
+                switch (comboBox2.Text)
+
+                {
+
+                    case "Pesos Argentinos (ARS)":
+                        MessageBox.Show("Error: No debes ingresar la misma moneda.");
+                        break;
+
+                    case "Pesos Mexicanos (MXN)":
+                        double monedaMx = double.Parse(txtCantidad.Text);
+                        monedaMx = monedaMx * 0.19447;
+                        textBox2.Text = monedaMx.ToString();
+                        break;
+
+                    case "Pesos Colombianos (COP)":
+                        double monedaCop = double.Parse(txtCantidad.Text);
+                        monedaCop = monedaCop * 37.476;
+                        textBox2.Text = monedaCop.ToString();
+                        break;
+
+                    case "Dólar (USD)":
+                        double monedaUsd = double.Parse(txtCantidad.Text);
+                        monedaUsd = monedaUsd * 0.0094500;
+                        textBox2.Text = monedaUsd.ToString();
+                        break;
+
+                    case "Dólar Canadiense (CAD)":
+                        double monedaCad = double.Parse(txtCantidad.Text);
+                        monedaCad = monedaCad * 0.011990;
+                        textBox2.Text = monedaCad.ToString();
+                        break;
+
+                    case "Dólar de Nueva Zelanda (NZD)":
+                        double monedaNzd = double.Parse(txtCantidad.Text);
+                        monedaNzd = monedaNzd * 0.014190;
+                        textBox2.Text = monedaNzd.ToString();
+                        break;
+
+                    case "Euro (EUR)":
+                        double monedaEur = double.Parse(txtCantidad.Text);
+                        monedaEur = monedaEur * 0.0082700;
+                        textBox2.Text = monedaEur.ToString();
+                        break;
+
+
+                }
+            }
+          if (cmbDivisas.Text == "Pesos Colombianos (COP)")
+            {
+                switch (comboBox2.Text)
+
+                {
+
+                    case "Pesos Colombianos (COP)":
+                        MessageBox.Show("Error: No debes ingresar la misma moneda.");
+                        break;
+
+                    case "Pesos Mexicanos (MXN)":
+                        double monedaMx = double.Parse(txtCantidad.Text);
+                        monedaMx = monedaMx * 0.0051900;
+                        textBox2.Text = monedaMx.ToString();
+                        break;
+
+                    case "Pesos Argentinos (ARS)":
+                        double monedaArg = double.Parse(txtCantidad.Text);
+                        monedaArg = monedaArg * 0.026680;
+                        textBox2.Text = monedaArg.ToString();
+                        break;
+
+                    case "Dólar (USD)":
+                        double monedaUsd = double.Parse(txtCantidad.Text);
+                        monedaUsd = monedaUsd * 0.00025000;
+                        textBox2.Text = monedaUsd.ToString();
+                        break;
+
+                    case "Dólar Canadiense (CAD)":
+                        double monedaCad = double.Parse(txtCantidad.Text);
+                        monedaCad = monedaCad * 0.00032000;
+                        textBox2.Text = monedaCad.ToString();
+                        break;
+
+                    case "Dólar de Nueva Zelanda (NZD)":
+                        double monedaNzd = double.Parse(txtCantidad.Text);
+                        monedaNzd = monedaNzd * 0.00038000;
+                        textBox2.Text = monedaNzd.ToString();
+                        break;
+
+                    case "Euro (EUR)":
+                        double monedaEur = double.Parse(txtCantidad.Text);
+                        monedaEur = monedaEur * 0.00022000;
+                        textBox2.Text = monedaEur.ToString();
+                        break;
+
+
+                }
+            }
+          if (cmbDivisas.Text == "Dólar (USD)")
+            {
+                switch (comboBox2.Text)
+
+                {
+
+                    case "Dólar (USD)":
+                        MessageBox.Show("Error: No debes ingresar la misma moneda.");
+                        break;
+
+                    case "Pesos Mexicanos (MXN)":
+                        double monedaMx = double.Parse(txtCantidad.Text);
+                        monedaMx = monedaMx * 20.577;
+                        textBox2.Text = monedaMx.ToString();
+                        break;
+
+                    case "Pesos Argentinos (ARS)":
+                        double monedaArg = double.Parse(txtCantidad.Text);
+                        monedaArg = monedaArg * 105.82;
+                        textBox2.Text = monedaArg.ToString();
+                        break;
+
+                    case "Pesos Colombianos (COP)":
+                        double monedaCop = double.Parse(txtCantidad.Text);
+                        monedaCop = monedaCop * 3.965;
+                        textBox2.Text = monedaCop.ToString();
+                        break;
+
+                    case "Dólar Canadiense (CAD)":
+                        double monedaCad = double.Parse(txtCantidad.Text);
+                        monedaCad = monedaCad * 1.2692;
+                        textBox2.Text = monedaCad.ToString();
+                        break;
+
+                    case "Dólar de Nueva Zelanda (NZD)":
+                        double monedaNzd = double.Parse(txtCantidad.Text);
+                        monedaNzd = monedaNzd * 1.5014;
+                        textBox2.Text = monedaNzd.ToString();
+                        break;
+
+                    case "Euro (EUR)":
+                        double monedaEur = double.Parse(txtCantidad.Text);
+                        monedaEur = monedaEur * 0.87514;
+                        textBox2.Text = monedaEur.ToString();
+                        break;
+
+
+                }
+            }
+          if (cmbDivisas.Text == "Dólar Canadiense (CAD)")
+            {
+                switch (comboBox2.Text)
+
+                {
+
+                    case "Dólar Canadiense (CAD)":
+                        MessageBox.Show("Error: No debes ingresar la misma moneda.");
+                        break;
+
+                    case "Pesos Mexicanos (MXN)":
+                        double monedaMx = double.Parse(txtCantidad.Text);
+                        monedaMx = monedaMx * 16.2121;
+                        textBox2.Text = monedaMx.ToString();
+                        break;
+
+                    case "Pesos Argentinos (ARS)":
+                        double monedaArg = double.Parse(txtCantidad.Text);
+                        monedaArg = monedaArg * 83.359;
+                        textBox2.Text = monedaArg.ToString();
+                        break;
+
+                    case "Pesos Colombianos (COP)":
+                        double monedaCop = double.Parse(txtCantidad.Text);
+                        monedaCop = monedaCop * 3.124;
+                        textBox2.Text = monedaCop.ToString();
+                        break;
+
+                    case "Dólar (USD)":
+                        double monedaUsd = double.Parse(txtCantidad.Text);
+                        monedaUsd = monedaUsd * 0.78776;
+                        textBox2.Text = monedaUsd.ToString();
+                        break;
+
+                    case "Dólar de Nueva Zelanda (NZD)":
+                        double monedaNzd = double.Parse(txtCantidad.Text);
+                        monedaNzd = monedaNzd * 1.18328;
+                        textBox2.Text = monedaNzd.ToString();
+                        break;
+
+                    case "Euro (EUR)":
+                        double monedaEur = double.Parse(txtCantidad.Text);
+                        monedaEur = monedaEur * 0.68962;
+                        textBox2.Text = monedaEur.ToString();
+                        break;
+
+
+                }
+            }
+          if (cmbDivisas.Text == "Dólar de Nueva Zelanda (NZD)")
+            {
+                switch (comboBox2.Text)
+
+                {
+
+                    case "Dólar de Nueva Zelanda (NZD)":
+                        MessageBox.Show("Error: No debes ingresar la misma moneda.");
+                        break;
+
+                    case "Pesos Mexicanos (MXN)":
+                        double monedaMx = double.Parse(txtCantidad.Text);
+                        monedaMx = monedaMx * 13.7010;
+                        textBox2.Text = monedaMx.ToString();
+                        break;
+
+                    case "Pesos Argentinos (ARS)":
+                        double monedaArg = double.Parse(txtCantidad.Text);
+                        monedaArg = monedaArg * 70.447;
+                        textBox2.Text = monedaArg.ToString();
+                        break;
+
+                    case "Pesos Colombianos (COP)":
+                        double monedaCop = double.Parse(txtCantidad.Text);
+                        monedaCop = monedaCop * 2.64079;
+                        textBox2.Text = monedaCop.ToString();
+                        break;
+
+                    case "Dólar (USD)":
+                        double monedaUsd = double.Parse(txtCantidad.Text);
+                        monedaUsd = monedaUsd * 0.66575;
+                        textBox2.Text = monedaUsd.ToString();
+                        break;
+
+                    case "Dólar Canadiense (CAD)":
+                        double monedaCad = double.Parse(txtCantidad.Text);
+                        monedaCad = monedaCad * 0.84511;
+                        textBox2.Text = monedaCad.ToString();
+                        break;
+
+                    case "Euro (EUR)":
+                        double monedaEur = double.Parse(txtCantidad.Text);
+                        monedaEur = monedaEur * 0.84511;
+                        textBox2.Text = monedaEur.ToString();
+                        break;
+
+
+                }
+            }
+          if (cmbDivisas.Text == "Euro (EUR)")
+            {
+                switch (comboBox2.Text)
+
+                {
+
+                    case "Euro (EUR)":
+                        MessageBox.Show("Error: No debes ingresar la misma moneda.");
+                        break;
+
+                    case "Pesos Mexicanos (MXN)":
+                        double monedaMx = double.Parse(txtCantidad.Text);
+                        monedaMx = monedaMx * 23.5088;
+                        textBox2.Text = monedaMx.ToString();
+                        break;
+
+                    case "Pesos Argentinos (ARS)":
+                        double monedaArg = double.Parse(txtCantidad.Text);
+                        monedaArg = monedaArg * 120.877;
+                        textBox2.Text = monedaArg.ToString();
+                        break;
+
+                    case "Pesos Colombianos (COP)":
+                        double monedaCop = double.Parse(txtCantidad.Text);
+                        monedaCop = monedaCop * 4.5312;
+                        textBox2.Text = monedaCop.ToString();
+                        break;
+
+                    case "Dólar (USD)":
+                        double monedaUsd = double.Parse(txtCantidad.Text);
+                        monedaUsd = monedaUsd * 1.14232;
+                        textBox2.Text = monedaUsd.ToString();
+                        break;
+
+                    case "Dólar Canadiense (CAD)":
+                        double monedaCad = double.Parse(txtCantidad.Text);
+                        monedaCad = monedaCad * 1.45008;
+                        textBox2.Text = monedaCad.ToString();
+                        break;
+
+                    case "Dólar de Nueva Zelanda (NZD)":
+                        double monedaNzd = double.Parse(txtCantidad.Text);
+                        monedaNzd = monedaNzd * 1.71585;
+                        textBox2.Text = monedaNzd.ToString();
+                        break;
+
+
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtCantidad.Text = "";
+            cmbDivisas.Text = "";
+            comboBox2.Text = "";
+            textBox2.Text = "";
+        }
+    }
+}
